8e29639538d3c31f9dea43337da8eff4b4db3e83
chananchida-srithongdee <63010187@kmitl.ac.th>
2022-10-14 01:58:24 +0700

    just .cs files

---
+++ ex03.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace OS_Sync_Ex_01
+{
+    class Program
+    {
+        private static int sum = 0;
+        private static object _Lock = new object();
+
+        static void plus()
+        {
+            int i;
+            lock (_Lock)
+            {
+                for (i = 0; i < 1000000; i++)
+                {
+                    sum++;
+                }
+            }
+        }
+
+        static void minus()
+        {
+            int i;
+            lock (_Lock)
+            {
+                for (i = 0; i < 1000000; i++)
+                {
+                    sum--;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread P = new Thread(new ThreadStart(plus));
+            Thread M = new Thread(new ThreadStart(minus));
+
+            Stopwatch sw = new Stopwatch();
+            Console.WriteLine("Start...");
+            sw.Start();
+
+            P.Start();
+            M.Start();
+
+            P.Join();
+            M.Join();
+
+            sw.Stop();
+            Console.WriteLine("sum = {0}", sum);
+            Console.WriteLine("Time used: " + sw.ElapsedMilliseconds.ToString() + "ms");
+        }
+    }
+}
