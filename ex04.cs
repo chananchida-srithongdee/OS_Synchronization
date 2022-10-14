ffce24e8ec4a0dbf76bda021546f1a0e392244a1
chananchida-srithongdee <63010187@kmitl.ac.th>
2022-10-14 16:49:04 +0700

    modified ex04

--- ex04.cs
+++ ex04.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading;
-
+// Processor	Intel(R) Core(TM) i7-10750H CPU @ 2.60GHz, 2592 Mhz, 6 Core(s), 12 Logical Processor(s)
 namespace OS_Sync_01
 {
     class Program
     {
-        private static string x = "";
+        private static string x = ""; //x is shared value
         private static int exitflag = 0;
+        private static bool inputflag = false;
+        private static object _Lock = new object();
         static void ThReadX()
         {
+            // while (exitflag == 0)
+            // {
+            //     if (inputflag == true)
+            //     {
+            //         lock (_Lock)
+            //         {
+            //             inputflag = false;
+            //             Console.WriteLine("x = {0}", x);
+            //         }
+            //     }
+            // }
             while (exitflag == 0)
             {
-                Console.WriteLine("X = {0}", x);
+                lock (_Lock)
+                {
+                    if (inputflag == true)
+                    {
+                        inputflag = false;
+                        Console.WriteLine("x = {0}", x);
+                    }
+                }
             }
         }
         static void ThWriteX()
@@ -20,20 +40,59 @@ namespace OS_Sync_01
             string xx;
             while (exitflag == 0)
             {
-                Console.Write("Input: ");
-                xx = Console.ReadLine();
-                if (xx == "exit")
-                    exitflag = 1;
-                else
-                    x = xx;
+
+                lock (_Lock)
+                {
+                    if (inputflag == false)
+                    {
+                        Console.Write("Input: ");
+                        xx = Console.ReadLine();
+                        if (xx == "exit")
+                        {
+                            exitflag = 1;
+                            Console.WriteLine("Thread {0} is exiting...", Thread.CurrentThread.ManagedThreadId);
+                        }
+                        else
+                        {
+                            lock (_Lock)
+                            {
+                                x = xx;
+                                inputflag = true;
+                            }
+                        }
+                    }
+                }
             }
+            // while (exitflag == 0)
+            // {
+            //     if (inputflag == false)
+            //     {
+            //         Console.Write("Input: ");
+            //         xx = Console.ReadLine();
+            //         if (xx == "exit")
+            //         {
+            //             exitflag = 1;
+            //             Console.WriteLine("Thread {0} is exiting...", Thread.CurrentThread.ManagedThreadId);
+            //         }
+            //         else
+            //         {
+            //             lock (_Lock)
+            //             {
+            //                 x = xx;
+            //                 inputflag = true;
+            //             }
+            //         }
+            //     }
+            // }
         }
         static void Main(string[] args)
         {
-            Thread A = new Thread(ThReadX);
-            Thread B = new Thread(ThWriteX);
+            Thread A = new Thread(new ThreadStart(ThReadX));
+            Thread B = new Thread(new ThreadStart(ThWriteX));
             A.Start();
             B.Start();
+            // Console.WriteLine("Thread A, ID = {0}", A.ManagedThreadId); //Thread A, ID = 4
+            // Console.WriteLine("Thread B, ID =  {0}", B.ManagedThreadId); //Thread B, ID =  5
         }
     }
 }
